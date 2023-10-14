a91c74c8e6b4052f9550a3cf0f6a7181ba3742f6
cppimprov <cppimprov@gmail.com>
2023-10-14 12:27:26 +0100

    Start writing tests for io code.

--- Code/bump/io/bump_io.test.cpp
+++ Code/bump/io/bump_io.test.cpp
@@ -7,11 +7,48 @@ namespace bump
 
 	namespace io
 	{
-	
-		TEST(testtest, test)
+
+		TEST(Test_bump_io_copy_span, empty_dst_empty_src)
 		{
-			ASSERT_EQ(1, 1);
+			auto const src = std::span<char const>{};
+			auto dst = std::span<char>{};
+			auto const result = copy_span(src, dst);
+			EXPECT_TRUE(result.has_value());
 		}
+	
+		TEST(Test_bump_io_copy_span, dst_too_small)
+		{
+			auto const src = std::span<char const>{ "hello world" };
+			auto dst = std::span<char>{};
+			auto const result = copy_span(src, dst);
+			EXPECT_FALSE(result.has_value());
+			EXPECT_EQ(result.error(), io_err::output_buffer_too_small);
+		}
+
+		TEST(Test_bump_io_copy_span, same_size_src_dst)
+		{
+			constexpr auto src = std::span<char const>{ "hello world" };
+			char out[std::size(src)]{ 0 };
+			auto dst = std::span<char>(out, std::size(out));
+			ASSERT_TRUE(std::size(src) == std::size(dst));
+			auto const result = copy_span(src, dst);
+			EXPECT_TRUE(result.has_value());
+			EXPECT_TRUE(std::equal(std::begin(src), std::end(src), std::begin(dst)));
+		}
+
+		TEST(Test_bump_io_copy_span, smaller_src)
+		{
+			constexpr auto src = std::span<char const>{ "hello world" };
+			char out[std::size(src) + 10]{ 0 };
+			auto dst = std::span<char>(out, std::size(out));
+			ASSERT_TRUE(std::size(src) < std::size(dst));
+			auto const result = copy_span(src, dst);
+			EXPECT_TRUE(result.has_value());
+			EXPECT_TRUE(std::equal(std::begin(src), std::end(src), std::begin(dst)));
+		}
+
+		// TODO: adjust the dst span after writing? or return number of bytes written?
+		// write some usage code and see what feels better
 	
 	} // io
 
