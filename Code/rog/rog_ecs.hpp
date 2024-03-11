65399ae0766f2592d3efd3480c37bdbb7230f4af
cppimprov <cppimprov@gmail.com>
2024-03-11 13:09:17 +0000

    ...

---
+++ Code/rog/rog_ecs.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "rog_screen.hpp"
+
+#include <bump_math.hpp>
+
+namespace rog
+{
+	
+	struct c_position { glm::ivec2 m_pos; };
+	struct c_visual { screen_cell m_cell; };
+	struct c_actor { std::int32_t m_energy = 0; };
+
+	struct c_player_tag { };
+	struct c_monster_tag { };
+	struct c_object_tag { };
+	
+} // rog
